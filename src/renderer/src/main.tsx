299e120aa90705ca9b290e0c6fc6b3b2454c6362
SeungrokYoon <sonicrok93@gmail.com>
2024-09-30 08:53:15 +0900

    :sparkles: Feat :  MemoryRouter사용

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,10 +3,10 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 
-const router = createBrowserRouter([
+const router = createMemoryRouter([
   { path: '/', element: <App /> },
   { path: '/login', element: <LoginPage /> }
 ])
